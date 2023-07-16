73e373bb0301e900eca94a3385d392e57c28e72d
maicol1912 <maicolarcila34@gmail.com>
2023-07-16 15:04:09 -0500

    IMPLEMENTING CIRCUIT BREAKER IN API GATEWAY

---
+++ CloudGateway/src/test/java/com/maicol1912/CloudGateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maicol1912.CloudGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
