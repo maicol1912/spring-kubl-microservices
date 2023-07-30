778a6394f216088b88564c0721bb0cafae8b1ad4
maicol1912 <maicolarcila34@gmail.com>
2023-07-29 23:38:13 -0500

    Add all test in the services

--- OrderService/src/main/java/com/maicol1912/OrderService/external/client/PaymentService.java
+++ OrderService/src/main/java/com/maicol1912/OrderService/external/client/PaymentService.java
@@ -15,7 +15,7 @@ public interface PaymentService {
     @PostMapping
     public ResponseEntity<Long>doPayment(@RequestBody PaymentRequest paymentRequest);
 
-    default void fallback(Exception e){
+    default ResponseEntity<Void> fallback(Exception e){
         throw new CustomException("Payment Service is not available",
                 "UNAVAILABLE",
                 500);
